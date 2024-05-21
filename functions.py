d1a2a87782a9848884d7167ff09a7fed1259892c
Gustavo Lacerda <lsbgustavo@gmail.com>
2024-05-21 18:55:25 -0300

    img and text input

--- functions.py
+++ functions.py
@@ -14,9 +14,15 @@ def import_file():
 	file_path = filedialog.askopenfilename(title="Select a file", filetypes=[('image files', ('.png', '.jpg', 'jpeg')), ("All files", "*.*")])
 	if file_path:
 		copy_and_rename(file_path, "./assets", "tempIMG.png")
+		getimg()
 		print("Selected file")
 
 def gettext(input):
 	inp = input.get(1.0, "end-1c")
 	print(inp) 
 
+def getimg():
+    global img
+    img = Image.open("./assets/tempIMG.png")
+    img = img.resize((250, 250))
+    img = ImageTk.PhotoImage(img)
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from tkinter import *
 from tkinter import ttk
 from functions import *
 
-
 root = Tk()
 root.geometry("800x400")
 
@@ -14,12 +13,7 @@ inputtxt.grid(column=0, row=1, columnspan=1, padx=3, pady=3)
 
 ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
 ttk.Button(frm, text="Select a file", command=import_file).grid(column=1, row=0)
-ttk.Button(frm, text="Texto", command=gettext(inputtxt)).grid(column=2, row=0)
-
-img = Image.open("./assets/tempIMG.png")
-img = img.resize((250, 250))
-img = ImageTk.PhotoImage(img)
-label1 = Label(image=img)
-label1.place(x=400, y=50)
+ttk.Button(frm, text="Texto", command=lambda: gettext(inputtxt)).grid(column=2, row=0)
+getimg()
 
 root.mainloop()
